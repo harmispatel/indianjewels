8a3146a1fc6dd44393ec14a52f52b534fd4bec22
harmistest <harmistest@gmail.com>
2023-06-26 10:57:38 +0530

    New Changes

--- resources/views/admin/layouts/admin-sidebar.blade.php
+++ resources/views/admin/layouts/admin-sidebar.blade.php
@@ -1,5 +1,6 @@
 <aside id="sidebar" class="sidebar">
     <ul class="sidebar-nav" id="sidebar-nav">
+
         <li class="nav-item">
             <a class="nav-link " href="{{ route('admin.dashboard') }}">
             <i class="bi bi-grid"></i>
@@ -8,13 +9,13 @@
         </li>
 
         <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
-                <i class="bi bi-layout-text-window-reverse"></i><span>Tables</span><i class="bi bi-chevron-down ms-auto"></i>
+            <a class="nav-link collapsed" data-bs-target="#catalogue-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-layout-text-window-reverse"></i><span>Catelogue</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
-            <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <ul id="catalogue-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                 <li>
                     <a href="#">
-                    <i class="bi bi-circle"></i><span>General Tables</span>
+                    <i class="bi bi-circle"></i><span>Categories</span>
                     </a>
                 </li>
                 <li>
