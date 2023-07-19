09acfb494076d9a324f6aca90374321a77d5f55c
HarmisAndroidDev <harmistest@gmail.com>
2023-07-19 09:52:11 +0530

    Changes

--- app/Http/Controllers/APIController/CustomerApiController.php
+++ app/Http/Controllers/APIController/CustomerApiController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers\APIController;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Http\Resources\ParentCategoryResource;
 
 class CustomerApiController extends Controller
 {
     //
+    public function parentcategory(Request $request)
+    {
+        $category  = Category::where('parent_category',$request->id)->get();
+        
+        $datas = new ParentCategoryResource($category);
+    }
 }
