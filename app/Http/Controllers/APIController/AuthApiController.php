30c9dba07cd6e1b9376aaff0b9299ea634909c3a
harmistest <harmistest@gmail.com>
2023-08-18 16:28:45 +0530

    Changes

--- app/Http/Controllers/APIController/AuthApiController.php
+++ app/Http/Controllers/APIController/AuthApiController.php
@@ -12,18 +12,18 @@ class AuthApiController extends Controller
     //
     public function userlogin(Request $request)
     {
-        if(Auth::guard('web')->attempt(['email' => $request->email, 'password' => $request->password])){ 
-            
+        if(Auth::guard('web')->attempt(['email' => $request->email, 'password' => $request->password])){
+
             $user = new CustomerResource(Auth::guard('web')->user());
 
-            $token =  $user->createToken('MyApp')-> accessToken; 
-        
+            $token =  $user->createToken('MyApp')-> accessToken;
+
             $data = ['user' => $user, 'token' => $token];
             return $this->sendResponse(true, 'User Logged In Successfully...', $data);
-        } 
-        else{ 
-            
-            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
-        } 
+        }
+        else{
+
+            return $this->sendApiResponse(false, 0,'Failed to Login!', (object)[]);
+        }
     }
 }
